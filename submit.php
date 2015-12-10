64241e3120e2a2414e17f204ae769575ea27f154
Jurre Kon <superknudde@hotmail.com>
2015-12-10 16:08:05 +0100

    My Reddit 2.1

--- submit.php
+++ submit.php
@@ -4,65 +4,117 @@
 <head>
     <title>My reddit</title>
     <link href="style.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+
+    <script>
+(function($,W,D)
+{
+    var JQUERY4U = {};
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#myform").validate({
+                rules: {
+                    autor: "required",
+                    title: {
+                        required: true,
+                    minlength: 10
+                    },
+                    url: {
+                        required: true,
+                        url: true
+                    },
+                    summary: {
+                        required: true,
+                        minlength: 100
+                    },
+                },
+                messages: {
+                    autor: "Please enter the author",
+                    title: {
+                        required: "Please provide a title",
+                        minlength: "Your title must be at least 10 characters long"
+                    },
+                    url: {
+                        required: "Please provide a url",
+                        url: "please give a valid url"
+                    },
+                    summary: {
+                        required: "Please provide a summary",
+                        minlength: "The summary must be aleast 100 characters long"
+                    },
+                  
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+})(jQuery, window, document);
+    
+    </script>
 </head>
 
 <body>
-<!--<?php include 'header.php';?>-->
+
 <div id="content">
 <?php
   $connection = mysqli_connect('localhost', 'root');
 	
   mysqli_select_db($connection, 'reddit');
 // define variables and set to empty values
-$autorErr = $titleErr = $urlErr = $summaryErr = "";
+
 $autor = $title = $url = $summary = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["autor"])) {
-     $autorErr = "Author is required";
+   if (!empty($_POST["autor"])) {
        
-   }else{
        $autor = $_POST["autor"];
        $autor = mysqli_real_escape_string($connection,$autor);
    }
-   if (empty($_POST["title"])) {
-     $titleErr = "Title is required";
-   } else{
+   if (!empty($_POST["title"])) {
+
        $title = $_POST["title"];
        $title = mysqli_real_escape_string($connection,$title);
    }
    
-  if (empty($_POST["url"])) {
-     $urlErr = "URL is required";
-   } else{
+  if (!empty($_POST["url"])) {
+    
        $url = $_POST["url"];
       $url = mysqli_real_escape_string($connection,$url);
   }
    
-   if (empty($_POST["summary"])) {
-     $summaryErr = "Summary is required";
-   }else{
+   if (!empty($_POST["summary"])) {
+
        $summary = $_POST["summary"];
        $summary = mysqli_real_escape_string($connection,$summary); 
    }
 }
-
 ?>
     
     <div class="content">
 <h2>Voeg uw eigen post toe.</h2>
 <p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   <p class="form">Auteur: <input class="auteur" type="text" name="autor" value="">
-   <span class="error">* <?php echo $autorErr;?></span>
+<form id="myform" method="post" novalidate="novalidate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   <p class="form">
+    Auteur: <input class="auteur" type="text" name="autor" value="">
+
    <br><br>
    Titel: <input class="titel" type="text" name="title" value="">
-   <span class="error">* <?php echo $titleErr;?></span>
+
    <br><br>
    URL: <input class="url" type="text" name="url" value="">
-   <span class="error">* <?php echo $urlErr;?></span>
+
    <br><br>
    Samenvatting: <textarea class="samenvatting" name="summary" rows="5" cols="40"></textarea>
-   <span class="error">* <?php echo $summaryErr;?></span>
+
    <br><br>
    <input type="submit" name="submit" value="Submit"> 
        </p>
@@ -82,12 +134,14 @@ foreach($required as $field) {
 if ($error) {
   echo "Nog niet alle velden zijn ingevuld";
 } else {
+    
 $sql = "INSERT INTO posts (autor, title, url, summary)
 VALUES ('$autor', '$title', '$url', '$summary')";
     echo "Post added";
+    
    header("Location: index.php"); 
     if ($connection->query($sql) === TRUE) {
-   echo "Post added";
+
 } else {
     echo "Error: " . $sql . "<br>" . $connection->error;
 }
@@ -95,6 +149,10 @@ VALUES ('$autor', '$title', '$url', '$summary')";
 }
      }
 ?>
+</div>
+</body>
+</html>
+
 </div>
 </body>
 </html>
